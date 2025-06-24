3c30be91feba7498dc205be05f5fb79367ec5c20
Daryna <rinabkh98@gmail.com>
2025-06-24 21:51:27 +0200

    add city search component & logo #1

--- src/components/Header /Header.tsx
+++ src/components/Header /Header.tsx
@@ -1,17 +1,21 @@
+import style from "./Header.module.scss";
 import { Search } from "../Search/Search";
+import { AlignRightOutlined } from "@ant-design/icons";
 
 export const Header = () => {
   return (
-    <header>
+    <header className={style.wrap}>
       <div className="container-big">
-        <div className="inner">
-          <div>
-            <img src="" alt="Logo" />
+        <div className={`inner ${style.inner}`}>
+          <div style={{ width: "200px" }}>
+            <img style={{ width: "inherit" }} src="/public/assets/images/logo.png" alt="Logo" />
           </div>
           <div>
             <Search />
           </div>
-          <div></div>
+          <div style={{ width: "100px" }}>
+            <AlignRightOutlined />
+          </div>
         </div>
       </div>
     </header>
